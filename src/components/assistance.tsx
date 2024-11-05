bd9743236e32111052a67dc25f9da6e4f9463e19
Yassine Belkacemi <yassinacho@yahoo.fr>
2024-11-05 04:46:17 -0500

    Update assistance.tsx

--- src/components/assistance.tsx
+++ src/components/assistance.tsx
@@ -10,24 +10,54 @@ import {
   Heading,
   SimpleGrid,
   Text,
+  useEditableControls,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Input,
+  ButtonGroup,
+  Flex,
 } from "@chakra-ui/react";
 import exercises from "../exercises.json";
 import { useState } from "react";
+import { CheckIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
 
 const SplitTable = ({
   assistance,
   split,
   rows,
+  setAssistance,
 }: {
   assistance: any[];
   split: string;
   rows: string[];
+  setAssistance: any;
 }) => {
-  const getFilteredExos = (split: string, group: string) => {
+  const getFilteredExos = (split: string, muscle: string) => {
     return assistance.filter(
-      (exo) => exo.split === split && exo.group[0] === group
+      (exo) => exo.split === split && exo.group[0] === muscle
     );
   };
+
+  function EditableControls() {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls();
+
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm">
+        <CheckIcon boxSize={3} mr={3} my={"auto"} {...getSubmitButtonProps()} />
+        <CloseIcon boxSize={3} mr={3} my={"auto"} {...getCancelButtonProps()} />
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent="center">
+        <EditIcon boxSize={3} mr={3} my={"auto"} {...getEditButtonProps()} />{" "}
+      </Flex>
+    );
+  }
 
   return (
     <Box p={5} pb={10} bgColor={"rgba(255, 99, 71, 0.75)"} borderRadius={3}>
@@ -43,14 +73,25 @@ const SplitTable = ({
             </Tr>
           </Thead>
           <Tbody>
-            {rows.map((row) => (
-              <Tr key={row}>
+            {rows.map((muscle) => (
+              <Tr key={muscle}>
                 <Td fontWeight={500} fontSize={17}>
-                  {row.charAt(0).toUpperCase() + row.slice(1)}
+                  {muscle.charAt(0).toUpperCase() + muscle.slice(1)}
                 </Td>
                 <Td>
-                  {getFilteredExos(split, row).map((exo) => (
-                    <Text key={exo.name}>{exo.name}</Text>
+                  {getFilteredExos(split, muscle).map((exo) => (
+                    <Editable
+                      key={exo.name}
+                      textAlign="center"
+                      defaultValue={exo.name}
+                      isPreviewFocusable={false}
+                      display={"flex"}
+                      flexDirection={"row"}
+                    >
+                      <EditableControls />
+                      <EditablePreview />
+                      <Input as={EditableInput} size={"sm"} />
+                    </Editable>
                   ))}
                 </Td>
               </Tr>
@@ -73,25 +114,29 @@ function Assistance() {
         Assistance exercises follow your main compound lifts and are organized
         into a five-day split: 2 Push days, 2 Pull days, and 1 Legs day.
         Typically, assistance exercises are performed in a 3x 8-12 rep range to
-        specifically target hypertrophy. You can customize the exercises
-        directly on the table to fit your needs.
+        specifically target hypertrophy. Here is a pool of assistance exercises
+        you can plug-&-play but you can always customize the exercises directly
+        on the table to fit your needs.
       </Text>
 
       <SimpleGrid columns={3} spacing={20}>
         <SplitTable
           assistance={assistance}
+          setAssistance={setAssistance}
           split="push"
           rows={["shoulders", "chest", "triceps"]}
         />
 
         <SplitTable
           assistance={assistance}
+          setAssistance={setAssistance}
           split="pull"
           rows={["back", "biceps", "forearms"]}
         />
 
         <SplitTable
           assistance={assistance}
+          setAssistance={setAssistance}
           split="legs"
           rows={["quadriceps", "hamstrings", "calves"]}
         />

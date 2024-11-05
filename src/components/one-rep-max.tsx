1e0aaf911f23298b99845b93f6569a9fc9e79ded
Yassine Belkacemi <yassinacho@yahoo.fr>
2024-11-05 04:46:19 -0500

    Update one-rep-max.tsx

--- src/components/one-rep-max.tsx
+++ src/components/one-rep-max.tsx
@@ -23,14 +23,15 @@ function OneRepMax({ maxes, setMaxes }) {
   return (
     <Box p={10}>
       <Text px={5} py={2} mb={10} borderRadius={5} bgColor={"gray.100"}>
-        A one-rep max (1RM) is the maximum weight you can lift for a single
-        repetition with proper form. To find your 1RM, start by warming up, then
-        choose a weight close to your max, and attempt a single lift. Increase
-        gradually until you reach the heaviest weight you can lift once without
-        breaking form. For a safer estimation, especially for beginners, you can
-        lift a lighter weight for several reps (e.g., 3-5) and use an online 1RM
-        calculator to estimate your max. Note: Make sure to enter all five maxes
-        for each lift in the program; any lift left blank will default to 0.
+        This program focuses on the five main compound lifts so it is important
+        to track them starting with your one-rep max. A one-rep max (1RM) is the
+        maximum weight you can lift for a single repetition with proper form. To
+        find your 1RM, start by warming up, then choose a weight close to your
+        max, and attempt a single lift. Increase gradually until you reach the
+        heaviest weight you can lift once without breaking form. For a safer
+        estimation, especially for beginners, you can lift a lighter weight for
+        several reps (e.g., 3-5) and use an online 1RM calculator to estimate
+        your max. Note: Any Lift left blank will default to 0.
       </Text>
 
       <SimpleGrid pt={10} columns={3} spacing={20}>

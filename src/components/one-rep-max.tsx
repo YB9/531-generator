5562d27044d380674ebafda01f236b778f0fc30f
Yassine Belkacemi <yassinacho@yahoo.fr>
2024-11-05 00:54:34 -0500

    Update one-rep-max.tsx

--- src/components/one-rep-max.tsx
+++ src/components/one-rep-max.tsx
@@ -1,15 +1,51 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  InputGroup,
+  InputRightElement,
+  NumberInput,
+  NumberInputField,
+  Text,
+} from "@chakra-ui/react";
 import exercises from "../exercises.json";
 
 function OneRepMax() {
   const lifts = exercises.filter((exercise) => exercise.category === "lift");
+
+  const handleChange = (value: string, lift: string) => {
+    console.log(value, lift);
+  };
+
   return (
-    <Box>
-      {lifts.map((lift) => (
-        <Text key={lift.name}>
-          {lift.name} - {lift.split}
-        </Text>
-      ))}
+    <Box p={10}>
+      <Text px={5} py={2} mb={10} borderRadius={5} bgColor={"gray.100"}>
+        A one-rep max (1RM) is the maximum weight you can lift for a single
+        repetition with proper form. To find your 1RM, start by warming up, then
+        choose a weight close to your max, and attempt a single lift. Increase
+        gradually until you reach the heaviest weight you can lift once without
+        breaking form. For a safer estimation, especially for beginners, you can
+        lift a lighter weight for several reps (e.g., 3-5) and use an online 1RM
+        calculator to estimate your max. Note: Make sure to enter all five maxes
+        for each lift in the program; any lift left blank will default to 0.
+      </Text>
+
+      <Flex direction={"column"} gap={10}>
+        {lifts.map((lift) => (
+          <NumberInput
+            w={350}
+            mx={"auto"}
+            onChange={(e) => handleChange(e, lift.name)}
+          >
+            <Text fontWeight={500} mb={2}>
+              {lift.name}
+            </Text>
+            <InputGroup>
+              <NumberInputField borderRadius={3} borderColor={"black"} />
+              <InputRightElement>{"lbs"}</InputRightElement>
+            </InputGroup>
+          </NumberInput>
+        ))}
+      </Flex>
     </Box>
   );
 }

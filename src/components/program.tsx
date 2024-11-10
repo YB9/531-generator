6da9f7c0a9408fa41a960eb57f3b551d28297c26
Yassine Belkacemi <yassinacho@yahoo.fr>
2024-11-10 18:25:14 -0500

    Update program.tsx

--- src/components/program.tsx
+++ src/components/program.tsx
@@ -1,8 +1,21 @@
-import { Text } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 
 function Program() {
-  return <Text>Program</Text>;
+  return (
+    <Box p={10}>
+      <Text px={5} py={2} mb={10} borderRadius={5} bgColor={"gray.100"}>
+        This program...
+      </Text>
+
+      <SimpleGrid pt={10} columns={5} spacing={20}>
+        <Box bg={"tomato"} p={5} borderRadius={5} />
+        <Box bg={"tomato"} p={5} borderRadius={5} />
+        <Box bg={"tomato"} p={5} borderRadius={5} />{" "}
+        <Box bg={"tomato"} p={5} borderRadius={5} />
+        <Box bg={"tomato"} p={5} borderRadius={5} />
+      </SimpleGrid>
+    </Box>
+  );
 }
-// todo: printable
-// todo: customizeable duration
+
 export default Program;
